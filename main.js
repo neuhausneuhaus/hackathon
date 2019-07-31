b25e59943cede4b8b3f9dfe5187eb58e2ec140c6
Courtney Kwong <courtneykwong@Courtneys-MacBook-Air.local>
2019-07-31 18:55:57 -0400

    start

---
+++ main.js
@@ -0,0 +1,13 @@
+//hackathon (newcourt)
+//idea: yt extension --> have videos on watch next/recommended to 'hover' dates or print in box;
+//approach: 
+//(1) 'scrape' web page (of video we're on) for recommended IDs;
+        //it's located in 'ytd-watch-next-secondary-results-render'
+            //...vi/YT_ID_HERE/hqd...
+//(2) YT API --> input IDs into search and populate data
+        //should search for date created and return those dates in order to us
+//(3) Append the API's results (dates) to the Original page's recommended videos (in order)
+
+
+
+
